1771f112a1a8c5a471e483a0f5b27ae8ef245475
bahary <Aly.bahary.ab@gmail.com>
2019-06-21 11:53:23 +0200

    0

---
+++ app/src/main/java/com/example/nihal/medeasy/Adapters/AssessmentSheetsModleAdapter.java
@@ -0,0 +1,67 @@
+package com.example.nihal.medeasy.Adapters;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.nihal.medeasy.Models.AssessmentSheetModel;
+import com.example.nihal.medeasy.Models.AssessmentSheetsModle;
+import com.example.nihal.medeasy.R;
+
+import java.util.List;
+
+public class AssessmentSheetsModleAdapter extends RecyclerView.Adapter < AssessmentSheetsModleAdapter.AssessmentSheetsModleAdapterHolder > {
+
+//      List<AssessmentSheetsModle> assessmentSheetsModleList;
+      List<AssessmentSheetModel> modelList;
+
+
+   /* public AssessmentSheetsModleAdapter(List<AssessmentSheetsModle> modles){
+        this.assessmentSheetsModleList = modles ;
+    }*/
+
+    public AssessmentSheetsModleAdapter(List<AssessmentSheetModel> modles){
+        this.modelList = modles ;
+    }
+    @NonNull
+    @Override
+    public AssessmentSheetsModleAdapterHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View row = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.assessment_sheets_modle_s,viewGroup,false) ;
+        AssessmentSheetsModleAdapterHolder holder = new AssessmentSheetsModleAdapterHolder(row) ;
+        return holder ;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AssessmentSheetsModleAdapterHolder assessmentSheetsModleAdapterHolder, int i) {
+
+      //  AssessmentSheetsModle modle = assessmentSheetsModleList.get(i)  ;
+        AssessmentSheetModel modle = modelList.get(i)  ;
+        assessmentSheetsModleAdapterHolder.elt5ssos.setText(modle.getYourComplaint());
+       // assessmentSheetsModleAdapterHolder.description.setText(modle.getDescription());
+       // assessmentSheetsModleAdapterHolder.drug.setText(modle.getDrug());
+        assessmentSheetsModleAdapterHolder.date.setText(modle.getDM());
+    }
+
+    @Override
+    public int getItemCount() {
+        return modelList.size() ;
+    }
+
+                        /*** class el Holder ***/
+
+    public  class AssessmentSheetsModleAdapterHolder extends RecyclerView.ViewHolder {
+        TextView elt5ssos, /*description, drug,*/ date;
+
+        public AssessmentSheetsModleAdapterHolder(@NonNull View itemView) {
+            super(itemView);
+            elt5ssos = itemView.findViewById(R.id.elt5ssos);
+          //  description = itemView.findViewById(R.id.description);
+          //  drug = itemView.findViewById(R.id.drug);
+            date = itemView.findViewById(R.id.date);
+        }
+    }
+
+}
